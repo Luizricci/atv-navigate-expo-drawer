7c46a3c94e0a9e3fa77b86eec3c0016d52e8d0ec
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-03-25 13:29:24 -0300

    Feat: create drawe navigate

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+
+import Drawer from "./navigation/drawerNavigator";
+
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Drawer />
+        </NavigationContainer>
+    );
+}
